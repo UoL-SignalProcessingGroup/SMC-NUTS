99540c618ab13d4caed6e1ac29bf6a01f27aed5f
Matthew Carter <m.j.carter2@liverpool.ac.uk>
2023-09-04 12:08:38 +0100

    fix: resample at each post-processing iteration

--- smcnuts/postprocessing/ess_tempering.py
+++ smcnuts/postprocessing/ess_tempering.py
@@ -5,14 +5,10 @@ def estimate_and_recycle(target, smcs):
     ess_logw = np.zeros([smcs.K+1, smcs.N])
     recycling_consntants = np.zeros([smcs.K+1, smcs.N])
     for k in range(smcs.K+1):
-        # Resample if we didn't resample at iteration k
-        if smcs.ess[k] < smcs.N / 2:
-            x = smcs.x_saved[k].copy()
-        else:
-            wn, _ = smcs.normalise_weights(smcs.logw_saved[k])
-            z = np.linspace(0, smcs.N-1, smcs.N, dtype=int)
-            z_new = smcs.rng.choice(z, smcs.N, p=wn)
-            x = smcs.x_saved[k].copy()[z_new]
+        wn, _ = smcs.normalise_weights(smcs.logw_saved[k])
+        z = np.linspace(0, smcs.N-1, smcs.N, dtype=int)
+        z_new = smcs.rng.choice(z, smcs.N, p=wn)
+        x = smcs.x_saved[k].copy()[z_new]
 
         ess_logw[k] = target.logpdf(x) - target.logpdf(x, phi=smcs.phi[k])
         ess_wn, _ = smcs.normalise_weights(ess_logw[k])
