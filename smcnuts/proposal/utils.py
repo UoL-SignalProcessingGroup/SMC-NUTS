59ed635f6c81832f7c8fc9631e6bfc830bd467d7
Lee <Lee.Devlin@liverpool.ac.uk>
2023-09-06 14:40:47 +0100

    Added documentation for Acc-Rej module, switch {U,K} to H0 and H1 to symbolize the Hamiltonian at the initial and proposed states, respectively.

--- smcnuts/proposal/utils.py
+++ smcnuts/proposal/utils.py
@@ -1,14 +1,34 @@
 import autograd.numpy as np
-from scipy.stats import multivariate_normal
 
+def hmc_accept_reject(target_lpdf, x, x_prime, r, r_prime, phi=1.0, rng=np.random.default_rng()):
+    
+    """ Calculate whether to accept or reject a move 
+        Returns a Boolean of True if the move is accepted and False if rejected.
 
-def hmc_accept_reject(target_lpdf, x, x_prime, v, v_prime, phi=1.0, rng=np.random.default_rng()):
+        Input:
+        target_lpdf: Target log pdf 
+        x: Intial state
+        x_prime: Proposed state
+        r: Initial momentum
+        r_prime: Proposed Momentum
+        phi: Temperature (default = 1.0)
+        rng: Random number generator
+
+        Output:
+        Boolean: True if accepted, False if rejected.
+
+    """
+
     with np.errstate(all='ignore'):
-        U = target_lpdf(x_prime, phi=phi) - (0.5 * np.dot(v_prime, v_prime))
-        K = target_lpdf(x, phi=phi) - (0.5 * np.dot(v, v))
-        acceptance_ratio = np.exp(U - K)
+        # Calculate the (log) Hamiltonians at the end and start of the proposal
+        H1 = target_lpdf(x_prime, phi=phi) - (0.5 * np.dot(r_prime, r_prime))
+        H0 = target_lpdf(x, phi=phi) - (0.5 * np.dot(r, r))
+        
+        # Calculate the acceptanace rate and probability
+        acceptance_ratio = np.exp(H1 - H0)
         acceptance_probability = min(1., acceptance_ratio)
 
+        # Generate a random nuber from a uniform distribution and compare to acceptance probability
         if rng.uniform() >  acceptance_probability or np.any(np.isinf(x_prime)):
             return False
         return True
