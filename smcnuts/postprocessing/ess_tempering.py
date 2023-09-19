5c067a9f97077f9a51f36bb2a8a995d5ce947ed3
Lee Devlin <Lee.Devlin@liverpool.ac.uk>
2023-09-19 18:06:16 +0100

    Additional commenting added

--- smcnuts/postprocessing/ess_tempering.py
+++ smcnuts/postprocessing/ess_tempering.py
@@ -16,13 +16,18 @@ def estimate_and_recycle(target, smcs):
     ess_logw = np.zeros([smcs.K+1, smcs.N])
     recycling_consntants = np.zeros([smcs.K+1, smcs.N])
     for k in range(smcs.K+1):
+        
+        # Using weights calculated in the the sampler draw a set a set of samples
         wn, _ = smcs.normalise_weights(smcs.logw_saved[k])
         z = np.linspace(0, smcs.N-1, smcs.N, dtype=int)
         z_new = smcs.rng.choice(z, smcs.N, p=wn)
         x = smcs.x_saved[k].copy()[z_new]
 
+	    #Calculate importance weights and normalise
         ess_logw[k] = target.logpdf(x) - target.logpdf(x, phi=smcs.phi[k])
         ess_wn, _ = smcs.normalise_weights(ess_logw[k])
+        
+        # Calculate mean and variance estimates, as well as recycling coefficients
         smcs.mean_estimate[k], smcs.variance_estimate[k] = smcs.estimate(x, ess_wn)
         recycling_consntants[k] = smcs.recycling.constant(ess_wn)
 
